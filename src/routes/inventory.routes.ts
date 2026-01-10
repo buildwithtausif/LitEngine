5acd42e4ea5fab28e84f7f5cbd4b0e4a48d501bb
Tausif Alam <devops.tausifalam@outlook.com>
2026-01-10 23:08:30 +0530

    Implemented GET:/api/inventory & ?id=UUID

--- src/routes/inventory.routes.ts
+++ src/routes/inventory.routes.ts
@@ -1,1 +1,8 @@
+import express, { Router } from "express";
+import inventoryController from "../controllers/inventory.ctrl.js";
 
+const inventory_router: Router = express.Router();
+const inventory = new inventoryController();
+
+inventory_router.route("/").get(inventory.readInventory)
+export default inventory_router;
