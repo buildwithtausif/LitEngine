87eb5e02e4c293ae58e140e0c650a0155f271a60
Tausif Alam <devops.tausifalam@outlook.com>
2026-01-17 18:10:11 +0530

    take 762 vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,8 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: "/dashboard/",
-  build: {
-    outDir: "../public/dashboard",
+build: {
+    outDir: "dist",
     emptyOutDir: true,
   },
   resolve: {
