425a90a73ef6e35199c6b8d116452f4ee84e9ee8
Tausif Alam <devops.tausifalam@outlook.com>
2026-01-17 15:57:16 +0530

    render troubleshoot

--- src/models/db.mjs
+++ src/models/db.mjs
@@ -1,29 +1,40 @@
 import pgPromise from "pg-promise";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 //import dotenv
 dotenv.config();
 // import db credentials from env
-const db_host = process.env.PSQL_HOST || 'localhost';
-const db_port = process.env.PSQL_PORT || '5600';
+const db_host = process.env.PSQL_HOST || "localhost";
+const db_port = process.env.PSQL_PORT || "5600";
 const db_user = process.env.PSQL_USER;
 const db_pass = process.env.PSQL_PASS;
 const db_name = process.env.PSQL_DB;
-const connectionQuery = `postgres://${db_user}:${db_pass}@${db_host}:${db_port}/${db_name}`;
-// const connectionQuery = `postgres://${db_user}:${db_pass}@localhost:5600/${db_name}`;
+const dbConfig = {
+  host: db_host.replace("https://", ""),
+  port: db_port,
+  database: db_name,
+  user: db_user,
+  password: db_pass,
+  allowExitOnIdle: true,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+};
+
 // init pg-promise and estabilish connection
 const pgp = pgPromise({
-    capSQL: true // to capitalize generated SQL
+  capSQL: true, // to capitalize generated SQL
 });
-const db = pgp(connectionQuery);
+const db = pgp(dbConfig);
 
 (async () => {
-    try {
-        const obj = await db.connect();
-        console.log(`db connected: ${obj.client.database}`);
-        obj.done();
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const obj = await db.connect();
+    console.log(`db connected: ${obj.client.database}`);
+    obj.done();
+  } catch (err) {
+    console.log(err);
+  }
 })();
 
 // export both as named exports, and db by default
