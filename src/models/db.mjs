0b0df9943b234b9e898f67b03235046b7f5cda85
Tausif Alam <devops.tausifalam@outlook.com>
2026-01-26 12:08:30 +0530

    added configuration for connection to db on cloud

--- src/models/db.mjs
+++ src/models/db.mjs
@@ -1,26 +1,57 @@
 import pgPromise from "pg-promise";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import getErrorMessage from "../utils/errorHandler.ts";
 //import dotenv
 dotenv.config();
 // import db credentials from env
+const db_host = process.env.PSQL_HOST;
+const db_port = process.env.PSQL_PORT;
 const db_user = process.env.PSQL_USER;
 const db_pass = process.env.PSQL_PASS;
 const db_name = process.env.PSQL_DB;
-const connectionQuery = `postgres://${db_user}:${db_pass}@localhost:5600/${db_name}`;
+const initOptions = {
+  capSQL: true, // to capitalize generated SQL
+  // Global event notification;
+  error(err, e) {
+    if (e.cn) {
+      // A connection-related error;
+      console.log("CN:", e.cn);
+      console.log("EVENT:", err.message || err);
+    }
+  },
+  connect(client, dc, useCount) {
+    const cp = client.connectionParameters;
+    console.log(
+      "Connected to database:",
+      cp?.database || client.database || db_name,
+    );
+  },
+};
+
 // init pg-promise and estabilish connection
-const pgp = pgPromise({
-    capSQL: true // to capitalize generated SQL
-});
-const db = pgp(connectionQuery);
+const pgp = pgPromise(initOptions);
+
+const dbConfig = {
+  host: db_host,
+  port: db_port,
+  database: db_name,
+  user: db_user,
+  password: db_pass,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  connectionTimeoutMillis: 200000, // return an error after 2 seconds if connection could not be established
+};
+console.log(dbConfig);
+const db = pgp(dbConfig);
 
 (async () => {
-    try {
-        const obj = await db.connect();
-        console.log(`db connected: ${obj.client.database}`);
-        obj.done();
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const obj = await db.connect();
+    console.log(`db connected: ${obj.client.database}`);
+    obj.done();
+  } catch (err) {
+    console.log(getErrorMessage(err));
+  }
 })();
 
 // export both as named exports, and db by default
